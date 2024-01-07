9f496f3429a2ff9cfa074e6833ba1f1e7cc68e8e
HackerOm <hackeromsutar@gmail.com>
2024-01-07 20:04:56 +0530

    Initial Commit By Mukund

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mukund 2ndProject"
+include(":app")
+ 
